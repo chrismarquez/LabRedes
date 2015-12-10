42ee14bfa2d03502e40fd2d8704abb827832f399
chrismarquez <a01225198@itesm.mx>
2015-12-09 19:35:11 -0600

    Project No Microsoft BS

--- WebApp/ReservaLab/wwwroot/js/Database.js
+++ WebApp/ReservaLab/wwwroot/js/Database.js
@@ -1,5 +1,6 @@
 var firebase = new Firebase("https://redestecgda.firebaseio.com/");
 
+
 function getReserved() {
 
 }
@@ -8,3 +9,17 @@ function setReservation() {
 
 }
 
+function extractValues() {
+	var dateString = $("#datepicker").val().split("-");
+	var year = dateString[0];
+	var month = dateString[1];
+	var day = dateString[2];
+	console.log(dateString);
+}
+
+$(function() {
+	$("#submit").click(function() {
+		extractValues();
+		return false;
+	});
+});
