3cd6388b88b75bfb64713797100c7439c43635c6
chrismarquez <a01225198@itesm.mx>
2015-12-11 16:12:23 -0600

    FormPage v0.1

--- WebApp/ReservaLab/wwwroot/js/Database.js
+++ WebApp/ReservaLab/wwwroot/js/Database.js
@@ -1,20 +1,52 @@
 var firebase = new Firebase("https://redestecgda.firebaseio.com/");
 
 
+function SimpleDate(dateArray) {
+	this.year = dateArray[0];
+	this.month = dateArray[1];
+	this.day = dateArray[2];
+}
+
+SimpleDate.prototype = {
+	constructor : SimpleDate,
+	getDay: function() {
+		return this.day;
+	},
+	getMonth: function() {
+		return this.month;
+	},
+	getYear: function() {
+		return this.year;
+	},
+	compareTo: function(date) { 
+		
+	}
+};
+
+function SimpleTime(timeArray) {
+	this.hour = timeArray[0];
+	this.min = timeArray[1];
+}
+
 function getReserved() {
 
 }
 
 function setReservation() { 
-
+	firebase.push({
+		time : extractValues(),
+		students : addStudents()
+	});
 }
 
 function extractValues() {
-	var dateString = $("#datepicker").val().split("-");
-	var year = dateString[0];
-	var month = dateString[1];
-	var day = dateString[2];
-	console.log(dateString);
+	var dateString = $("#datepicker").val();
+	var date = new SimpleDate(dateString.split("-"));
+	console.log(date.getDay());
+	var startTime = $("#startTime").val();
+	var endTime = $("#endTime").val();
+	return [dateString, startTime, endTime];
+
 }
 function addItem() {
 	var item = $("#item").val();
@@ -24,13 +56,24 @@ function addItem() {
 		$("#addedItems").html(last + item + ": " + number);
 		$("#itemNumber").val("0");
 		$("#item").val("default");
+	}	
+}
+
+function addStudents() {
+	var data = $("#students").val().split("\n");
+	var students = [];
+	for (var i = 0; i < data.length; i++) {
+		var split = data[i].indexOf("A0");
+		var name = data[i].substring(0, split - 1);
+		var id = data[i].substring(split, data[i].length);
+		students[i] = {name: name, id : id};
 	}
-	
+	return students;
 }
 
 $(function() {
 	$("#submit").click(function() {
-		extractValues();
+		setReservation();
 		return false;
 	});
 	$("#submitItem").click(function() {
